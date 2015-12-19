a508fcf8797a5a57e6def319b366a2ec1ddd2c5e
Leland Miller <lelandmiller@gmail.com>
2015-12-19 01:00:10 -0800

    Initial scaffolding and import
    Imported sources from previous site and created gulp based build system.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,70 @@
 import gulp from 'gulp';
+import sass from 'gulp-sass';
+import markdown from 'gulp-markdown';
+import connect from 'gulp-connect';
+import mergeStream from 'merge-stream';
+import frontMatter from 'gulp-front-matter';
+import es from 'event-stream';
+import jade from 'jade';
+import browserSyncModule from 'browser-sync';
+import ghPages from 'gulp-gh-pages';
 
-gulp.task('deploy', () => {
-    console.log('hello');
+const browserSync = browserSyncModule.create();
+
+function browserSyncStart() {
+    browserSync.init({
+        server: 'build',
+        directory: true,
+        ghostMode: false
+    });
+}
+
+
+gulp.task('deploy', ['build'], () => {
+    return gulp.src('build/**/*').pipe(ghPages({
+        branch: 'master'
+    }));
 });
 
+gulp.task('build', () => outputStream().pipe(gulp.dest('build')));
 
+gulp.task('rebuild', ['build'], browserSync.reload);
+
+gulp.task('watch', ['rebuild'], () => {
+    gulp.watch('src/**/*', ['build']);
+});
+
+gulp.task('serve', browserSyncStart);
+
+gulp.task('dev', ['serve', 'watch']);
+
+function getTemplate(name) {
+    return jade.compileFile('src/templates/' + name + '.jade');
+}
+
+function applyTemplate(file, cb) {
+    const templateName = file.data.template || 'default';
+    const template = getTemplate(templateName);
+    file.contents = new Buffer(template({
+        content: file.contents.toString(),
+        data: file.data
+    }));
+    cb(null, file);
+}
+
+function outputStream() {
+    const styles =  gulp.src('src/styles/main.scss')
+        .pipe(sass({outputStyle: 'compressed'}));
+
+    const content = gulp.src('src/content/**/*.md')
+        .pipe(frontMatter({
+            property: 'data',
+            remove: true
+        }))
+        .pipe(markdown())
+        .pipe(es.map(applyTemplate));
+
+    const other = gulp.src('src/static/**/*');
+
+    return mergeStream(styles, content, other);
+}
---
+++ src/content/index.md
@@ -0,0 +1,34 @@
+---
+title: Home
+---
+
+About Me
+========
+
+<img class="profile-picture" alt="My Face" src="https://s.gravatar.com/avatar/4b7aa5d70e65996e472a27483d254497?s=80" />
+
+Hello! My name is Leland Miller. I enjoy spending time with my family and getting outdoors when I have the chance. As far as computers go, I always enjoy learning about programming languages, operating systems, and anything else about how computers work or could work in the future. The design of programming languages and information storage systems is particularly interesting to me as there are many different approaches to these problems and solutions borrow ideas from a vast set of interdisciplinary fields.
+
+<table class="profiles"><tr>
+<td>
+<a href="https://github.com/lelandmiller">
+<img src="/images/GitHub-Mark-64px.png" width="32" height="32" border="0" alt="View Leland Miller's profile on GitHub" />
+</a>
+</td>
+<td>
+<a href="https://www.linkedin.com/pub/leland-miller/18/170/574">
+<img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_liprofile_blue_80x15.png" width="80" height="15" border="0" alt="View Leland Miller's profile on LinkedIn">
+</a>
+</td></tr></table>
+
+Projects
+--------
+
+- SlugCam is a solar-powered wireless smart camera system. I have been involved as a researcher on this project. <http://www.slugcam.com>, <https://github.com/SlugCam>
+- TextScape is a language I designed and for a comparative programming languages class at UCSC. I implemented an interpreter in Haskell and a simple IDE that is served by a Ruby WEBrick server. <https://github.com/lelandmiller/TextScape>
+
+Music
+-----
+
+- The Initialized is a project I started in 2009. There are a few albums out there, the project is no longer active after doing some recording and playing a few shows. <https://theinitialized.bandcamp.com/>
+
